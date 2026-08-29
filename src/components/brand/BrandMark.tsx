9cb3a9c1d2550323b17994de6f0b873bcfcb723f
0bytedna <0bytedna@gmail.com>
2026-08-29 17:34:49 +0530

    golden clour

--- src/components/brand/BrandMark.tsx
+++ src/components/brand/BrandMark.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { cn } from "@/lib/cn";
 
 export function BrandMark({
@@ -12,25 +13,14 @@ export function BrandMark({
       aria-hidden
       className={cn("relative inline-flex size-9 shrink-0", className)}
     >
-      <svg
-        viewBox="0 0 512 512"
-        role="presentation"
-        className="size-full"
-        data-priority={priority || undefined}
-      >
-        <g fill="#d4af37">
-          <path d="M 184 300 L 239 274 L 239 390 L 184 390 Z" />
-          <path d="M 252 251 L 307 225 L 307 390 L 252 390 Z" />
-          <path d="M 320 207 L 375 181 L 375 390 L 320 390 Z" />
-        </g>
-        <path
-          d="M 377 145 A 153 181 0 1 0 373 372"
-          fill="none"
-          stroke="#0f192d"
-          strokeWidth="66"
-          strokeLinecap="butt"
-        />
-      </svg>
+      <Image
+        src="/brand/chopra-capital-mark-c2-gold.png"
+        alt=""
+        fill
+        sizes="48px"
+        priority={priority}
+        className="object-contain"
+      />
     </span>
   );
 }
